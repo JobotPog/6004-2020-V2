c1a573e6f47778df77424cec33a816bfca6e12ba
6004 <me@here.com>
2020-01-22 18:14:57 -0500

    initial load

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,92 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.XboxController;
+//import frc.robot.commands.*;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+  public Joystick _driver = new Joystick(RobotMap.OI_DRIVER_CONTROLLER); 
+  public XboxController _operator = new XboxController(RobotMap.OI_OP_CONTROLLER);
+  public XboxController _squishy = new XboxController(RobotMap.OI_SQUISHY);
+
+  /*public Button LADfrontClimb = new JoystickButton(_operator, 2);
+  public Button LADrearClimb = new JoystickButton(_operator, 3);
+  public Button LADfrontDrop = new JoystickButton(_operator, 5);
+  public Button LADrearDrop = new JoystickButton(_operator, 4);
+  public Button LADallClimb = new JoystickButton(_operator, 6);
+  public Button LADDriveFwd = new JoystickButton(_operator, 1);
+  public Button SquishyEndoIn = new JoystickButton(_operator, 7);
+  public Button SquishyEndoOut = new JoystickButton(_operator, 8);
+  public Button SquishyClawOpen = new JoystickButton(_operator, 9);
+  public Button SquishyClawClose = new JoystickButton(_operator, 10);
+*/
+//public Button LADDriveFwd = new JoystickButton(_operator, 1);
+public Button LADDriveFwd = new JoystickButton(_operator, 6);
+public Button LADHold = new JoystickButton(_operator, 5);
+public Button LADRearHold = new JoystickButton(_operator, 3);
+public Button LADUpdateAll = new JoystickButton(_operator, 4);
+
+//NEEDS TESTING, not sure button map
+  public Button SquishyTiltUp = new JoystickButton(_squishy, 4);
+  public Button SquishyTiltDown = new JoystickButton(_squishy, 1);
+  public Button SquishyClawOpen = new JoystickButton(_squishy, 5);
+  public Button SquishyClawClose = new JoystickButton(_squishy, 6);
+//^^^^^^^^
+
+public Button CloseSol = new JoystickButton(_driver,5);
+public Button OpenSol = new JoystickButton(_driver,6);
+
+  public OI () {
+//GOOD
+/*
+    LADfrontClimb.whileHeld(new LADFrontUpdate(-RobotMap.FRONT_SPEED));
+    LADrearClimb.whileHeld(new LADRearUpdate(-RobotMap.REAR_SPEED));
+    
+    LADfrontDrop.whileHeld(new LADFrontUpdate(RobotMap.FRONT_SPEED));
+    LADrearDrop.whileHeld(new LADRearUpdate(RobotMap.REAR_SPEED));
+    */
+    /*
+    LADDriveFwd.whileHeld(new LADUpdateDrive(RobotMap.DRIVE_SPEED));
+    LADHold.whileHeld(new LADAllLift(.15,-.15));
+    LADRearHold.whileHeld(new LADRearUpdate(-.15));
+    LADUpdateAll.whileHeld(new LADAllLift(RobotMap.FRONT_SPEED,-RobotMap.REAR_SPEED));
+    CloseSol.whenPressed(new SolClose());
+    OpenSol.whenPressed(new SolOpen());
+    */
+
+//NEEDS TESTING
+/*
+    LADallClimb.whileHeld(new LADAllLift(RobotMap.FRONT_SPEED,RobotMap.REAR_SPEED));    
+
+    SquishyEndoIn.whileHeld(new squishyIntake(RobotMap.INTAKE_SPEED));
+    SquishyEndoOut.whileHeld(new squishyExhaust(RobotMap.EXHAUST_SPEED));
+    SquishyClawOpen.whileHeld(new squishyClawOpen());
+    SquishyClawClose.whileHeld(new squishyClawClose());
+*/
+/*
+    SquishyTiltUp.whileHeld(new squishyTiltUpdate(-RobotMap.TILT_SPEED));
+    SquishyTiltDown.whileHeld(new squishyTiltUpdate(RobotMap.TILT_SPEED));
+    SquishyClawClose.whileHeld(new squishyClawClose(-RobotMap.TILT_SPEED));
+    SquishyClawOpen.whileHeld(new squishyClawOpen(RobotMap.TILT_SPEED));
+    */
+
+    //RunClimbDown.whileHeld(new climbdown());    
+    //RunDriveBack.whileHeld(new climbdriveback());
+    //UpFront.whileHeld(new climbupdown());
+    //UpBak.whileHeld(new climbbackup());
+    
+    
+  }
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,140 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.ctre.phoenix.motorcontrol.InvertType;
+
+import frc.robot.OI;
+//import frc.robot.subsystems.*;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import com.ctre.phoenix.motorcontrol.can.*;
+
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  WPI_TalonSRX _leftMaster = new WPI_TalonSRX(11);
+  WPI_TalonSRX _rightMaster = new WPI_TalonSRX(10);
+  WPI_VictorSPX  _leftFollow = new WPI_VictorSPX (13);
+  WPI_VictorSPX  _rightFollow = new WPI_VictorSPX (12);
+  DifferentialDrive _drive = new DifferentialDrive(_leftMaster, _rightMaster);
+    
+  public static OI m_oi;
+
+  
+
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    _leftMaster.configFactoryDefault();
+    _rightMaster.configFactoryDefault();
+    _leftFollow.configFactoryDefault();
+    _rightFollow.configFactoryDefault();
+    
+    _leftFollow.follow(_leftMaster);
+    _rightFollow.follow(_rightMaster);
+    
+    _leftMaster.setInverted(false); // <<<<<< Adjust this until robot drives forward when stick is forward
+    _rightMaster.setInverted(true); // <<<<<< Adjust this until robot drives forward when stick is forward
+    _leftFollow.setInverted(InvertType.FollowMaster);
+    _rightFollow.setInverted(InvertType.FollowMaster);
+    _drive.setRightSideInverted(false); // do not change this
+
+    //ALWAYS LAST!!!!!!!!!!!!!!!!!!!!!!!!!!
+    m_oi = new OI();
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+    //Scheduler.getInstance().run();
+    
+    double forward = 1 * m_oi._driver.getY();
+    double turn = m_oi._driver.getTwist();
+    _drive.arcadeDrive(-forward, turn);
+
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
+}

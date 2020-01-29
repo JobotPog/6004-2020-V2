296a2539d6fa4651cd0954c326b753bcbe08399d
Matthew Williams <matthew@oasyssports.com>
2020-01-28 19:33:41 -0500

    add in first run of LED

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,6 +27,8 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import com.ctre.phoenix.motorcontrol.can.*;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 
 
 /**
@@ -50,6 +52,12 @@ public class Robot extends TimedRobot {
 
   VictorSPX Shooter = new VictorSPX(5);
   CANSparkMax Pivot = new CANSparkMax(6, MotorType.kBrushless);
+
+  private AddressableLED m_led;
+  private AddressableLEDBuffer m_ledBuffer;
+  // Store what the last hue of the first pixel is
+  private int m_rainbowFirstPixelHue;
+
   public static OI m_oi;
 
   /**
@@ -77,6 +85,21 @@ public class Robot extends TimedRobot {
     _leftFollow.setInverted(InvertType.FollowMaster);
     _rightFollow.setInverted(InvertType.FollowMaster);
     _drive.setRightSideInverted(false); // do not change this
+    
+
+     // PWM port 9
+    // Must be a PWM header, not MXP or DIO
+    m_led = new AddressableLED(9);
+
+    // Reuse buffer
+    // Default to a length of 60, start empty output
+    // Length is expensive to set, so only set it once, then just update data
+    m_ledBuffer = new AddressableLEDBuffer(300);
+    m_led.setLength(m_ledBuffer.getLength());
+
+    // Set the data
+    m_led.setData(m_ledBuffer);
+    m_led.start();
 
     //ALWAYS LAST!!!!!!!!!!!!!!!!!!!!!!!!!!
     m_oi = new OI();
@@ -159,5 +182,25 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
+    // Fill the buffer with a rainbow
+    rainbow();
+    // Set the LEDs
+    m_led.setData(m_ledBuffer);
   }
+
+  private void rainbow() {
+    // For every pixel
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Calculate the hue - hue is easier for rainbows because the color
+      // shape is a circle so only one value needs to precess
+      final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+      // Set the value
+      m_ledBuffer.setHSV(i, hue, 255, 128);
+    }
+    // Increase by to make the rainbow "move"
+    m_rainbowFirstPixelHue += 3;
+    // Check bounds
+    m_rainbowFirstPixelHue %= 180;
+  }
+  
 }
